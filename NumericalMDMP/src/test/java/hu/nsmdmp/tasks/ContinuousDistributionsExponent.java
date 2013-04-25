e2f75d52ff015a176bcc05ebf19c34d8799771f2
gmnagy@gmail.com <gmnagy@gmail.com@4107d651-a4c8-415a-9f25-286c730fa24a>
2013-04-25 11:45:14 +0000

    Itt nézem a cikk szerint előállított precíz megoldásokat is. Máshol is így kellene...
    git-svn-id: svn://localhost/tmp/testgc@94 4107d651-a4c8-415a-9f25-286c730fa24a

--- NumericalMDMP/src/test/java/hu/nsmdmp/tasks/ContinuousDistributionsExponent.java
+++ NumericalMDMP/src/test/java/hu/nsmdmp/tasks/ContinuousDistributionsExponent.java
@@ -6,16 +6,21 @@ import static hu.nsmdmp.polynomialmatrixfactory.ChebyshevUMatrix.generateApfloat
 import static hu.nsmdmp.specialvectors.Discrete.discreteVector;
 import static hu.nsmdmp.tasks.TaskUtils.getMaxCumProbMatrixElement;
 import static hu.nsmdmp.tasks.TaskUtils.getMinCumProbMatrixElement;
+import static hu.nsmdmp.tasks.TaskUtils.getPrecMaxCumProbMatrixElement;
+import static hu.nsmdmp.tasks.TaskUtils.getPrecMinCumProbMatrixElement;
 import static hu.nsmdmp.tools.SetNormalization.normalize;
 import static hu.nsmdmp.tools.SetVariationIterator.getNumberOfVariation;
 import static hu.nsmdmp.tools.SubSequencesGenerator.getSubSequences2;
 import hu.nsmdmp.moments.Moment;
 import hu.nsmdmp.mosek.LPSolution;
+import hu.nsmdmp.mosek.PreciseLPSolution;
+import hu.nsmdmp.mosek.PreciseLPCalc;
 import hu.nsmdmp.numerics.matrix.Matrix;
 import hu.nsmdmp.numerics.matrix.Vector;
 import hu.nsmdmp.utils.IOFile;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
@@ -35,7 +40,7 @@ public class ContinuousDistributionsExponent {
 		Apfloat[] pArray = IOFile.read(new File(ContinuousDistributionsExponent.class.getResource(pFile).toURI()), Apfloat.class);
 
 		int n = 23; //?
-		int m = 5;
+		int m = 3;
 		int dim = 2;
 		int l = 11;
 
@@ -68,20 +73,43 @@ public class ContinuousDistributionsExponent {
 			result[ind][1] = String.valueOf(minLPSolution.getSolutionStatus());
 			result[ind][2] = String.valueOf((System.currentTimeMillis() - startTime));
 
+			System.out.println("start-minPrec");
+			startTime = System.currentTimeMillis();
+			PreciseLPSolution<Apfloat> minPrecLPSolution = getPrecMinCumProbMatrixElement(chebU, normChebyUV, f);
+			System.out.println("Running time-min:" + (System.currentTimeMillis() - startTime));
+
+			
 			System.out.println("start-max");
 			startTime = System.currentTimeMillis();
 			LPSolution maxLPSolution = getMaxCumProbMatrixElement(chebU, normChebyUV, f);
 			System.out.println("Running time-max:" + (System.currentTimeMillis() - startTime));
-			System.out.println(String.format("min:%s  -  max:%s", minLPSolution.getPrimalSolution(), maxLPSolution.getPrimalSolution()));
-
+			
 			result[ind][3] = String.valueOf(maxLPSolution.getPrimalSolution());
 			result[ind][4] = String.valueOf(maxLPSolution.getSolutionStatus());
 			result[ind][5] = String.valueOf((System.currentTimeMillis() - startTime));
+			
+			System.out.println("start-maxPrec");
+			startTime = System.currentTimeMillis();
+			PreciseLPSolution<Apfloat> maxPrecLPSolution = getPrecMaxCumProbMatrixElement(chebU, normChebyUV, f);
+			System.out.println("Running time-min:" + (System.currentTimeMillis() - startTime));
+
+			
+			System.out.println(String.format("min:%s  -  max:%s", minLPSolution.getPrimalSolution(), maxLPSolution.getPrimalSolution()));
+			System.out.println(String.format("Prec_min:%s  -  Prec_max:%s", minPrecLPSolution.getObjectiveValue(), maxPrecLPSolution.getObjectiveValue()));
+			System.out.println(String.format("nonneg_min:%s  -  nonneg_max:%s",
+					minPrecLPSolution.getPrimalNonnegInfeas().doubleValue(), maxPrecLPSolution.getPrimalNonnegInfeas().doubleValue()));
+			System.out.println(String.format("prslack_min:%s  -  prslack_max:%s", 
+					minPrecLPSolution.getPrimalSlackInfeas().doubleValue(), maxPrecLPSolution.getPrimalSlackInfeas().doubleValue()));
+			System.out.println(String.format("dualslack_min:%s  -  dslack_max:%s", 
+					minPrecLPSolution.getDualSlackInfeas().doubleValue(), maxPrecLPSolution.getDualSlackInfeas().doubleValue()));
+			//System.out.println(  Arrays.toString(minLPSolution.getBasisIndexes()) + Arrays.toString(maxLPSolution.getBasisIndexes()));
+
+
 
 			ind++;
 		}
 
-		IOFile.write("resultCDE", "/t", result);
+		IOFile.write("resultCDE", "\t", result);
 	}
 
 	private static Vector<Apfloat> createPowerMoments(Apfloat p, Double[] expArray, int n, int m, int dim, int l) throws Exception {
