2922a4d6827a75e8f226225527371e11fbd6b466
mng <mng@TOSH-portege>
2016-10-20 14:50:40 +0200

    test has been corrected

--- NumericalMDMP/src/test/java/hu/nsmdmp/moments/ArrayCombinationTest.java
+++ NumericalMDMP/src/test/java/hu/nsmdmp/moments/ArrayCombinationTest.java
@@ -3,6 +3,9 @@ package hu.nsmdmp.moments;
 import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.assertArrayEquals;
 
+import java.util.Arrays;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class ArrayCombinationTest {
@@ -11,16 +14,20 @@ public class ArrayCombinationTest {
 	public void testCombinations1() {
 
 		SubSet[] result = MultivariateMoments.combinations(new int[] { 0, 1 }, 1, new int[] { 0, 1 });
-
+		
+//		System.out.println(Arrays.toString(result));
+//		System.out.println(Arrays.toString(new SubSet[] { new SubSet(0, 1), new SubSet(0, 3), new SubSet(1, 3) }));
+		
 		assertEquals(2, result.length);
 		assertArrayEquals(new SubSet[] { new SubSet(0), new SubSet(1) }, result);
 	}
 
+//	@Ignore
 	@Test
 	public void testCombinations2() {
 
-		SubSet[] result = MultivariateMoments.combinations(new int[] { 0, 1, 3 }, 2, new int[] { 0, 1, 3 });
-
+//		SubSet[] result = MultivariateMoments.combinations(new int[] { 0, 1, 3 }, 2, new int[] { 0, 1, 3 });
+		SubSet[] result = MultivariateMoments.combinations(new int[] { 0, 1, 2 }, 2, new int[] { 0, 1, 3 });
 		assertEquals(3, result.length);
 		assertArrayEquals(new SubSet[] { new SubSet(0, 1), new SubSet(0, 3), new SubSet(1, 3) }, result);
 	}
